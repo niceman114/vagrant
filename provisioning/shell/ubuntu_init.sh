ff9da58520ab5a95abd9e2e16c4388e375a920a1
sgoh <niceman114@gmail.com>
2013-10-23 00:35:49 +0900

    improved provisioning/shell/ubuntu_init.sh

--- provisioning/shell/ubuntu_init.sh
+++ provisioning/shell/ubuntu_init.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+sudo -s
+
 # set localtime
-sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
-sudo apt-get -y install rdate
-sudo rdate -s time.bora.net
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+apt-get -y install rdate
+rdate -s time.bora.net
 
 # update system packages
-sudo apt-get purge -y vim-tiny
-sudo apt-get update 
-sudo apt-get install -y vim
-sudo apt-get install -y openssl
+apt-get purge -y vim-tiny
+apt-get update 
+apt-get install -y vim
+apt-get install -y openssl
 
 # set .vimrc
 echo -e "set autoindent
@@ -21,14 +24,33 @@ set softtabstop=4
 set tabstop=4
 set tenc=utf-8
 set visualbell
-
 colorscheme slate
 filetype indent on
-syntax on
-" > .vimrc
+syntax on" > .vimrc
 
 # set history format
 export HISTTIMEFORMAT='[%F %T] '
 declare -r HISTFILE
 
-# sudo service mysql restart
+# set editor from nano to vim
+update-alternatives --set editor /usr/bin/vim.basic
+
+if [ "$1" == "jvm" ]; then
+		
+	cp /vagrant/provisioning/download/jdk-7u45-linux-i586.gz /tmp/
+	cd /tmp
+	mkdir -p /usr/local/jdk
+	tar xvfz jdk-7u45-linux-i586.gz
+	mv jdk1.7.0_45 /usr/local/jdk/
+
+	update-alternatives --install /usr/bin/java java /usr/local/jdk/jdk1.7.0_45/bin/java 1
+	update-alternatives --set java /usr/local/jdk/jdk1.7.0_45/bin/java
+	update-alternatives --install /usr/bin/javac javac /usr/local/jdk/jdk1.7.0_45/bin/javac 1
+	update-alternatives --set javac /usr/local/jdk/jdk1.7.0_45/bin/javac
+
+	echo -e "JAVA_HOME=/usr/local/jdk/jdk1.7.0_45
+	PATH=\$PATH:\$JAVA_HOME/bin" > /etc/profile.d/jvm.sh
+
+fi
+
+exit
