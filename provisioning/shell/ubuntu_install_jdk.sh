3aaa190f38ff5bf9767f38c29a191e55e6698f46
sgoh <niceman114@gmail.com>
2013-10-24 18:52:52 +0900

    rename and improve : ubuntu_install_jvm.sh -> ubuntu_install_jdk.sh

--- provisioning/shell/ubuntu_install_jdk.sh
+++ provisioning/shell/ubuntu_install_jdk.sh
@@ -2,9 +2,11 @@
 /vagrant/provisioning/shell/ubuntu_init.sh
 
 sudo -s
-cp /vagrant/provisioning/download/jdk-7u45-linux-i586.gz /tmp/
+
 cd /tmp
+
 mkdir -p /usr/local/jdk
+cp /vagrant/provisioning/download/jdk-7u45-linux-i586.gz .
 tar xvfz jdk-7u45-linux-i586.gz
 chown -R root.root jdk1.7.0_45
 mv jdk1.7.0_45 /usr/local/jdk/
@@ -14,8 +16,14 @@ update-alternatives --set java /usr/local/jdk/jdk1.7.0_45/bin/java
 update-alternatives --install /usr/bin/javac javac /usr/local/jdk/jdk1.7.0_45/bin/javac 1
 update-alternatives --set javac /usr/local/jdk/jdk1.7.0_45/bin/javac
 
-echo -e "JAVA_HOME=/usr/local/jdk/jdk1.7.0_45
-CLASSPATH=$JAVA_HOME/lib
-PATH=\$PATH:\$JAVA_HOME/bin" > /etc/profile.d/jvm.sh
+echo "export JAVA_HOME=/usr/local/jdk/jdk1.7.0_45" >> /etc/profile
+# echo "export CLASSPATH=\$JAVA_HOME/lib" >> /etc/profile
+echo "export PATH=\$PATH:\$JAVA_HOME/bin" >> /etc/profile
 
-exit
+# echo -e "JAVA_HOME=/usr/local/jdk/jdk1.7.0_45
+# CATALINA_HOME=/usr/local/tomcat/apache-tomcat-7.0.42
+# CLASSPATH=$JAVA_HOME/lib
+# PATH=\$PATH:\$JAVA_HOME/bin
+# " > /etc/profile.d/jvm.sh
+
+exit	
